c198743e050345aac07cafbd3c6a552f349d20cb
Athena Niimura <daniela.c.lopez1988@gmail.com>
2023-04-26 18:19:27 -0300

    se agrego usuario, contraseña y edad

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-let nombreUsuario = prompt ("Como te llamas?")
+let edad = prompt ("Ingresa tu edad")
 
-alert("Hola " + nombreUsuario)
+if (edad >= 18){
+    alert ("Sos mayor, podes ingresar");
+    let nombreUsuario = prompt ("Ingresa tu nombre de Usuario")
+    let pass = prompt ("Ingresa tu Contraseña")
+    if (nombreUsuario == "athena" && pass == "basurita") {
+        alert ("Ingresaste al sistema")
+    } else {
+        alert ("Datos incorrectos")
+    }
+} else {
+    alert ("Sos menor de edad")
+}
